c0d63e967718bd7d129567ca71b354e01d9dc5d5
Parkjju <rudwns3927@gmail.com>
2023-02-22 11:03:49 +0900

    feat: 스캔버튼 pressed 이벤트 관리

--- ViewController.swift
+++ ViewController.swift
@@ -7,21 +7,47 @@
 
 import UIKit
 import VisionKit
+import Vision
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var scanButton: UIButton!
-    @IBOutlet weak var imageVIew: UIImageView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var images: Array<Any> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        images = []
+        visionKit()
+        
+    }
+    
+    func visionKit(){
+        let scan = VNDocumentCameraViewController()
+        scan.delegate = self
+        self.present(scan, animated: true)
+    }
+    
 }
 
 
 extension ViewController: VNDocumentCameraViewControllerDelegate{
-    
+    func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
+        for pageNumber in 0 ..< scan.pageCount{
+            let image = scan.imageOfPage(at: pageNumber)
+            images.append(image)
+        }
+        
+        pageControl.numberOfPages = images.count
+        pageControl.currentPage = 0
+        imageView.image = (images[0] as! UIImage)
+        
+        controller.dismiss(animated: true)
+    }
 }
